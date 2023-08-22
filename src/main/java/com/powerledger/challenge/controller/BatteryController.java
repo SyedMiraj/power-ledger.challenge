d8b217279e614399df5301a4d40899e255a5eb0e
Shahriar  Miraj <smiraj2507@gmail.com>
2023-08-23 00:36:07 +0600

    Initial commit

---
+++ src/main/java/com/powerledger/challenge/controller/BatteryController.java
@@ -0,0 +1,7 @@
+package com.powerledger.challenge.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/batteries")
+public class BatteryController {
+}
