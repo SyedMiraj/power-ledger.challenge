825e479268a5451821395d00211a112807b78e81
Shahriar  Miraj <smiraj2507@gmail.com>
2023-08-25 09:51:38 +0600

    pipeline development

--- src/test/java/com/powerledger/challenge/ChallengeApplicationTests.java
+++ src/test/java/com/powerledger/challenge/ChallengeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.powerledger.challange;
+package com.powerledger.challenge;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ChallengeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
+
